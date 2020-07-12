994de29edc7dc7d14914482dc4c5544834022933
Bhushan Rajput <bhushanrajput2298@gmail.com>
2020-07-12 13:41:49 +0530

    Updates

---
+++ precache-manifest.0bd52bc492dfb9c77b2d6d2c507c1184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "986557ec5d7b433283f1be83e90a15f4",
+    "url": "/video-search/index.html"
+  },
+  {
+    "revision": "07cbc45728856b2b5c46",
+    "url": "/video-search/static/css/main.f9d7a250.chunk.css"
+  },
+  {
+    "revision": "5cb62341c774e81bb3fb",
+    "url": "/video-search/static/js/2.e4f53aa6.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/video-search/static/js/2.e4f53aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07cbc45728856b2b5c46",
+    "url": "/video-search/static/js/main.415c473e.chunk.js"
+  },
+  {
+    "revision": "f87c4187ae8346ed7653",
+    "url": "/video-search/static/js/runtime-main.7ff99981.js"
+  }
+]);
